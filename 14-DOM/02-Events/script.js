bdf2b537720d8b63d88318848fde2c3701ace568
mungaig <geoffiscoding@gmail.com>
2025-02-19 20:54:26 +0300

    Add event functions files

--- 14-DOM/02-Events/script.js
+++ 14-DOM/02-Events/script.js
@@ -1,3 +1,9 @@
 //second button code
 const btn = document.querySelector("#btn");
 btn.onclick = () => alert("Hello World");
+
+//Third button code
+const btn1 = document.querySelector('#btn1');
+btn1.addEventListener('click', () => {
+    alert('Hello World');
+})
