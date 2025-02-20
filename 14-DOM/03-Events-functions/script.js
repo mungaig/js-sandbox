68977b4f6e27cd3c0247e91139a78b3380cfa752
mungaig <geoffiscoding@gmail.com>
2025-02-20 15:27:22 +0300

    Add e and e.target exercise files

--- 14-DOM/03-Events-functions/script.js
+++ 14-DOM/03-Events-functions/script.js
@@ -6,12 +6,22 @@ function alertFunction(){
 //Method 2
 const btn2 = document.querySelector('#btn2');
 btn2.onclick = alertFunction;
+//get type of event using a callback function
+btn2.addEventListener('click', function(e){
+    console.log(e);
+})
+
 
 //Method 3 - preferred method
 const btn3 = document.querySelector('#btn3');
 btn3.addEventListener('click', alertFunction);
 
-//callback
+//get type of event using a callback function
 btn3.addEventListener('click', function(e){
+    console.log(e);
+})
+
+//get type of event using a callback function
+btn1.addEventListener('click', function(e){
     console.log(e);
 })
