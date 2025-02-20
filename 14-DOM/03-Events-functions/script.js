5a88a651ee717052c304f0920dd9556417d022d3
mungaig <geoffiscoding@gmail.com>
2025-02-20 08:18:37 +0300

    Add alert function, inline call and onclick methods

---
+++ 14-DOM/03-Events-functions/script.js
@@ -0,0 +1,11 @@
+//function
+function alertFunction(){
+ alert('You did it!');
+}
+
+//Method 2
+const btn2 = document.querySelector('#btn2');
+btn2.onclick = alertFunction;
+
+//Method 3
+const btn3 = document.querySelector('#btn3');
