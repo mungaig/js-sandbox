f7ab1b20519866a3e8a31ead1878ce636b376ad9
mungaig <geoffiscoding@gmail.com>
2025-02-26 11:30:31 +0300

    Make changes to <a> element

---
+++ 14-DOM/06-Active-Learning-DOM/script.js
@@ -0,0 +1,6 @@
+//Store element reference in a variable
+const link = document.querySelector('a');
+//change text ~ Node.textContent
+link.textContent = 'Mozilla Developer Network';
+//Change link address
+link.href = 'https://developer.mozilla.org';
